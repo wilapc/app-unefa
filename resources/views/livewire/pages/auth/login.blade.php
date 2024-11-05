de0512cefc1330d12a26d2febefbfa2cf6c2a337
Wilmer Pereira <wilmerapc26@gmail.com>
2024-11-04 23:40:40 -0400

    Ajustes Varios

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -7,65 +7,71 @@ use Livewire\Volt\Component;
 
 new #[Layout('layouts.guest')] class extends Component
 {
-    public LoginForm $form;
+  public LoginForm $form;
 
-    /**
-     * Handle an incoming authentication request.
-     */
-    public function login(): void
-    {
-        $this->validate();
+  /**
+   * Handle an incoming authentication request.
+   */
+  public function login(): void
+  {
+    $this->validate();
 
-        $this->form->authenticate();
+    $this->form->authenticate();
 
-        Session::regenerate();
+    Session::regenerate();
 
-        $this->redirectIntended(default: route('dashboard', absolute: false), navigate: true);
-    }
+    $this->redirectIntended(default: route('dashboard', absolute: false), navigate: true);
+  }
 }; ?>
 
-<div>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<div class="p-5">
+  <!-- Session Status -->
+  <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form wire:submit="login">
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Correo')" />
-            <x-text-input wire:model="form.email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('form.email')" class="mt-2" />
-        </div>
+  <form wire:submit="login">
+    <!-- Email Address -->
+    <div>
+      <x-input-label for="email" :value="__('Correo')" />
+      <x-text-input wire:model="form.email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus autocomplete="username" />
+      <x-input-error :messages="$errors->get('form.email')" class="mt-2" />
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Contraseña')" />
+    <!-- Password -->
+    <div class="mt-4">
+      <x-input-label for="password" :value="__('Contraseña')" />
 
-            <x-text-input wire:model="form.password" id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+      <x-text-input wire:model="form.password" id="password" class="block mt-1 w-full"
+        type="password"
+        name="password"
+        required autocomplete="current-password" />
 
-            <x-input-error :messages="$errors->get('form.password')" class="mt-2" />
-        </div>
+      <x-input-error :messages="$errors->get('form.password')" class="mt-2" />
+    </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember" class="inline-flex items-center">
-                <input wire:model="form.remember" id="remember" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Recordarme') }}</span>
-            </label>
-        </div>
+    <!-- Remember Me -->
+    <div class="block mt-4">
+      <label for="remember" class="inline-flex items-center">
+        <input wire:model="form.remember" id="remember" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+        <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Recordarme') }}</span>
+      </label>
+    </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}" wire:navigate>
-                    {{ __('Olvidó contraseña?') }}
-                </a>
-            @endif
+    <div class="flex sm:flex-wrap items-center justify-end mt-4">
+      @if (Route::has('password.request'))
+      <a class="underline text-sm my-3 px-3 text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}" wire:navigate>
+        {{ __('Olvidó contraseña?') }}
+      </a>
+      @endif
 
-            <x-primary-button class="ms-3">
-                {{ __('Entrar') }}
-            </x-primary-button>
-        </div>
-    </form>
+      @if (Route::has('register'))
+      <x-button-link href="{{ route('register')}}" class="my-3" wire:navigate>
+        {{ __('Registrar')}}
+      </x-button-link>
+      @endif
+
+      <x-primary-button class="ms-3 my-3">
+        {{ __('Entrar') }}
+      </x-primary-button>
+    </div>
+  </form>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,22 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        'login': "url('../../image/unefa_fondo.jpg')",
+      },
     },
+  },
 
-    plugins: [forms],
+  plugins: [forms],
 };
